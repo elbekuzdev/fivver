8c91714eb9c1baf41770fa50d54e7c029038d0cf
abdulloh <mainaccofabdulloh@gmail.com>
2022-11-20 00:27:40 +0500

    Created Validator for phoneNumber and added validation for userDto

--- auth/src/main/java/com/example/auth/dto/UsersDto.java
+++ auth/src/main/java/com/example/auth/dto/UsersDto.java
@@ -1,5 +1,6 @@
 package com.example.auth.dto;
 
+import com.example.auth.annotation.ValidatePhoneNumber;
 import com.example.auth.entity.District;
 import com.example.auth.entity.Region;
 import lombok.AllArgsConstructor;
@@ -7,18 +8,26 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class UsersDto {
     private Integer Id;
-    private String firstname;
-    private String lastname;
+    @NotBlank(message = "FirstName is mandatory")
+    private String firstName;
+    @NotBlank(message = "Lastname is mandatory")
+    private String lastName;
+    @NotBlank(message = "Profession is mandatory")
     private String profession;
     private String summary;
     private Region region;
     private District district;
+    @ValidatePhoneNumber
     private String phoneNumber;
+    @Email(message = "Email is not valid")
     private String email;
     private CommonsMultipartFile profilePicture;
 
