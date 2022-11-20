c384a231e6134b7e0133a41bdcd3bcd6326cbb37
elbek_uzdev <elbeknurmatov01@gmail.com>
2022-11-20 22:51:07 +0500

    main bitdi

---
+++ main/src/main/java/com/example/main/entity/Users.java
@@ -0,0 +1,68 @@
+package com.example.main.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
+
+import javax.persistence.*;
+import java.util.Collection;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Users implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int Id;
+    private String firstname;
+    private String lastname;
+    private String profession;
+    private String summary;
+    private String password;
+    @ManyToOne
+    private Region region;
+    @ManyToOne
+    private District district;
+    private String phoneNumber;
+    private String email;
+    private CommonsMultipartFile profilePicture;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
