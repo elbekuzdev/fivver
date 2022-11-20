b24d37fd20c75cd93fcc3c11abf9a61218b1b19b
abdulloh <mainaccofabdulloh@gmail.com>
2022-11-20 20:53:41 +0500

    SECURITY

---
+++ main/src/main/java/com/example/main/repo/RefreshTokenRepo.java
@@ -0,0 +1,12 @@
+package com.example.main.repo;
+
+import com.example.main.entity.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RefreshTokenRepo extends JpaRepository<RefreshToken,Integer> {
+    Optional<RefreshToken> findByRefreshToken(String refreshToken);
+}
