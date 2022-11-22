a9cf52b3e88de1b8bb00c873b1bb862e342b3be4
elbek_uzdev <elbeknurmatov01@gmail.com>
2022-11-23 01:18:55 +0500

    update yangilanish bo'ldi, update hali ish ko'p

--- auth/src/main/java/com/example/auth/mapper/LinksMapper.java
+++ auth/src/main/java/com/example/auth/mapper/LinksMapper.java
@@ -3,13 +3,33 @@ package com.example.auth.mapper;
 import com.example.auth.dto.LinksDto;
 import com.example.auth.entity.Links;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class LinksMapper {
-    public static Links toEntity(LinksDto linksdto){
-        return new Links(linksdto.getId(),linksdto.getName(),linksdto.getUrl(),
-                linksdto.getUsers());
+    public static Links toEntity(LinksDto linksdto) {
+        if (linksdto != null) return new Links(linksdto.getId(), linksdto.getName(), linksdto.getUrl());
+        return new Links();
     }
-    public static LinksDto toDto(Links links){
-        return new LinksDto(links.getId(),links.getName(),
-                links.getUrl(),links.getUser());
+
+    public static Set<Links> toEntity(Set<LinksDto> linksDtos) {
+        HashSet<Links> links = new HashSet<>();
+        if (linksDtos != null) for (LinksDto linksDto : linksDtos)
+            links.add(toEntity(linksDto));
+        return links;
     }
+
+    public static LinksDto toDto(Links links) {
+        if (links != null) return new LinksDto(links.getId(), links.getName(), links.getUrl());
+        return new LinksDto();
+    }
+
+    public static Set<LinksDto> toDto(Set<Links> links) {
+        HashSet<LinksDto> linksDtos = new HashSet<>();
+        if (links != null) for (Links link : links)
+            linksDtos.add(toDto(link));
+        return linksDtos;
+    }
+
+
 }
--- auth/src/main/java/com/example/auth/service/UserService.java
+++ auth/src/main/java/com/example/auth/service/UserService.java
@@ -2,24 +2,20 @@ package com.example.auth.service;
 
 import com.example.auth.dto.UsersDto;
 import com.example.auth.entity.District;
+import com.example.auth.entity.Links;
 import com.example.auth.entity.Region;
 import com.example.auth.entity.Users;
 import com.example.auth.mapper.UsersMapper;
 import com.example.auth.dto.ResponseDto;
 import com.example.auth.repo.DistrictRepo;
+import com.example.auth.repo.LinksRepository;
 import com.example.auth.repo.RegionRepo;
 import com.example.auth.repo.UsersRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
+import java.util.*;
 
 @RequiredArgsConstructor
 @Service
@@ -30,51 +26,62 @@ public class UserService {
     private final UsersRepository usersRepository;
     private final RegionRepo regionRepo;
     private final DistrictRepo districtRepo;
+    private final LinksRepository linksRepository;
+
     public ResponseDto register(UsersDto usersDto) {
         try {
             Users user = UsersMapper.toEntity(usersDto);
-            if (!usersRepository.existsByEmail(user.getEmail())){
+            if (!usersRepository.existsByEmail(user.getEmail())) {
                 String phoneNumber = user.getPhoneNumber();
-                if (!usersRepository.existsByPhoneNumber(phoneNumber)){
-
-
-                    if (user.getDistrict() != null && user.getRegion() != null){
+                if (!usersRepository.existsByPhoneNumber(phoneNumber)) {
+                    if (user.getDistrict() != null && user.getRegion() != null) {
                         Optional<District> optionalDistrict = districtRepo.findById(user.getDistrict().getId());
-                        if (optionalDistrict.isPresent()){
+                        if (optionalDistrict.isPresent()) {
                             District district = optionalDistrict.get();
                             Optional<Region> optionalRegion = regionRepo.findById(user.getRegion().getId());
-                            if (optionalRegion.isPresent()){
+                            if (optionalRegion.isPresent()) {
                                 Region region = optionalRegion.get();
-                                if (Objects.equals(district.getRegion().getId(), region.getId())){
+                                if (Objects.equals(district.getRegion().getId(), region.getId())) {
                                     try {
+                                        Set<Links> links = null;
+                                        if (user.getLinks() != null) {
+                                            links = user.getLinks();
+                                        } else {
+                                            links = new HashSet<>();
+                                        }
+                                        user.setLinks(links);
+                                        linksRepository.saveAll(links);
+                                        user.setPassword(passwordEncoder.encode(user.getPassword()));
+                                        System.out.println(user.getLinks());
                                         Users save = usersRepository.save(user);
-                                        return new ResponseDto(200, "saved", save);
-                                    }catch (Exception e){
+                                        return new ResponseDto(200, "ok", save);
+
+
+                                    } catch (Exception e) {
                                         e.printStackTrace();
                                         return ResponseDto.getSuccess(205, "not saved");
-
                                     }
 
-                                }else {
+                                } else {
                                     return ResponseDto.getSuccess(205, "district not attributed to region");
                                 }
-                            }else{
+                            } else {
                                 return ResponseDto.getSuccess(205, "region not found");
                             }
-                        }else{
-                            return ResponseDto.getSuccess(205,"district not found");
+                        } else {
+                            return ResponseDto.getSuccess(205, "district not found");
                         }
-                    }else{
+                    } else {
                         return ResponseDto.getSuccess(205, "region or district is invalid");
                     }
 
-                }else{
+                } else {
                     return ResponseDto.getSuccess(205, "phone number is already is eixsts");
                 }
-            }else{
+            } else {
                 return ResponseDto.getSuccess(205, "email is already is exists");
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             return ResponseDto.getSuccess(205, "not saved");
         }
@@ -85,21 +92,81 @@ public class UserService {
         return ResponseDto.getSuccess(all);
     }
 
-    public ResponseDto getUserById(Integer id) {
-        Optional<Users> byId = usersRepository.findById(id);
-        Users users = byId.get();
-        return ResponseDto.getSuccess(users);
-    }
-
 
     public ResponseDto loginUser(String email, String password) {
         Optional<Users> users = usersRepository.findByEmail(email);
-        if (users.isPresent() && passwordEncoder.matches(password,users.get().getPassword())){
+        if (users.isPresent() && passwordEncoder.matches(password, users.get().getPassword())) {
             return ResponseDto.getSuccess(users.get());
-        }return ResponseDto.UserNotFound();
+        }
+        return ResponseDto.UserNotFound();
     }
-    public ResponseDto deleteById(Integer id){
+
+    public ResponseDto deleteById(Integer id) {
         usersRepository.deleteById(id);
-        return ResponseDto.getSuccess(200,"succesfully deleted");
+        return ResponseDto.getSuccess(200, "successfully deleted");
+    }
+
+    public ResponseDto update(UsersDto usersDto) {
+        {
+            System.out.println(usersDto.getLinks());
+            Users user = UsersMapper.toEntity(usersDto);
+            System.out.println(user.getLinks());
+            if (user.getDistrict() != null && user.getRegion() != null) {
+                Optional<District> optionalDistrict = districtRepo.findById(user.getDistrict().getId());
+                if (optionalDistrict.isPresent()) {
+                    District district = optionalDistrict.get();
+                    Optional<Region> optionalRegion = regionRepo.findById(user.getRegion().getId());
+                    if (optionalRegion.isPresent()) {
+                        Region region = optionalRegion.get();
+                        if (Objects.equals(district.getRegion().getId(), region.getId())) {
+                            try {
+                                Optional<Users> optionalUser = usersRepository.findById(usersDto.getId());
+                                if (optionalUser.isPresent()) {
+                                    Users users = optionalUser.get();
+                                    Set<Links> links = null;
+                                    if (user.getLinks() != null) {
+                                        links = user.getLinks();
+                                    } else {
+                                        links = new HashSet<>();
+                                    }
+                                    if (users.getLinks() != null) {
+                                        links.addAll(users.getLinks());
+                                    }
+                                    user.setLinks(links);
+
+                                    user.setPassword(passwordEncoder.encode(user.getPassword()));
+                                    linksRepository.saveAll(links);
+                                    Users save = user;
+                                    try {
+                                        save = usersRepository.save(user);
+                                    }catch (Exception e){
+                                        e.printStackTrace();
+                                    }
+                                    return new ResponseDto(200, "ok", save);
+
+                                } else {
+                                    return ResponseDto.getSuccess(205, "user not found");
+                                }
+
+
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                                return ResponseDto.getSuccess(205, "not saved");
+                            }
+
+                        } else {
+                            return ResponseDto.getSuccess(205, "district not attributed to region");
+                        }
+                    } else {
+                        return ResponseDto.getSuccess(205, "region not found");
+                    }
+                } else {
+                    return ResponseDto.getSuccess(205, "district not found");
+                }
+            } else {
+                return ResponseDto.getSuccess(205, "region or district is invalid");
+            }
+
+        }
     }
 }
